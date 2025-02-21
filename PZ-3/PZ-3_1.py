af480fb34a97806780c905e1fd783793a341a397
student <di4906587@gmail.com>
2025-02-21 12:55:42 +0300

    added PZ-3_1

---
+++ PZ-3/PZ-3_1.py
@@ -0,0 +1,16 @@
+def check(string):
+    a = input(string)
+    while type(a) != int:
+        try:
+            a = int(a)
+        except ValueError:
+            print("Ведите целое число!")
+            a = int(input(string))
+        return a
+
+
+num_user = check("Введите положительное число:")
+if num_user > 0:
+    print("Число положительное - верно")
+else:
+    print("Число отрицательное - не верно")
