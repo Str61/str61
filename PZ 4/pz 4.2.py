b50a3afa24c44cd01dd1e461982cfedbdaf25db9
student <di4906587@gmail.com>
2025-03-21 12:58:28 +0300

    nenenen

---
+++ PZ 4/pz 4.2.py
@@ -0,0 +1,14 @@
+a = int(input("Введите число: "))
+b = int(input("Введите число: "))
+c = int(input("Введите число: "))
+square = 0
+while True:
+    if b > 0:
+        square += a
+        b -= 1
+    else:
+        break
+limit = c
+mini_square
+while True
+
