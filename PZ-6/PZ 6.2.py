9bbc75d67d73c4a110a210de0f9f2d8f5009a9bd
student <di4906587@gmail.com>
2025-04-11 16:23:18 +0300

    okda

--- PZ-6/PZ 6.2.py
+++ PZ-6/PZ 6.2.py
@@ -2,3 +2,18 @@
 #правого соседа, и количество таких элементов. Найденные номера выводить в
 #порядке их возрастания.
 
+def spisok(numbers):
+    indices = list()
+    count = 0
+    for i in range(0, len(numbers)-1):
+        if numbers[i] > numbers[i + 1]:
+            print("-----")
+            indices.append(numbers.index(i))
+            count += 1
+        return indices, count
+
+
+numbers = [1,5,2,8,3,7,4]
+result_indices,result_count = spisok(numbers)
+print("Номера элементов:", result_indices)
+print("Количество:",result_count)
